9b4b02c06cdc3fed86b944fc7db01f80805e151f
Harsh Mangalam <57381638+harshmangalam@users.noreply.github.com>
2021-03-15 12:11:35 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+// you can configure dotenv in one place it will work properly every file do not need to configure dotenv
 require("dotenv").config();
 
 const db = process.env.DB_URL || "mongodb://localhost:27017/fakebooker";
